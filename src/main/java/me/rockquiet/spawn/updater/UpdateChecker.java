4820038964df82b0c29915bf49a843ea3a88fef6
ChunghwaMC <92585552+ChunghwaMC@users.noreply.github.com>
2025-02-23 18:49:02 +0800

    add folia supported

--- src/main/java/me/rockquiet/spawn/updater/UpdateChecker.java
+++ src/main/java/me/rockquiet/spawn/updater/UpdateChecker.java
@@ -3,6 +3,7 @@ package me.rockquiet.spawn.updater;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import me.rockquiet.spawn.Spawn;
+import org.bukkit.Bukkit;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,44 +14,93 @@ import java.net.URL;
 public class UpdateChecker {
 
     public UpdateChecker(Spawn plugin) {
-        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
-            try {
-                URL url = new URL("https://api.github.com/repos/rockquiet/spawn/releases/latest");
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        if (Spawn.isFolia)
+            Bukkit.getAsyncScheduler().runNow(plugin, run -> {
+                try {
+                    URL url = new URL("https://api.github.com/repos/rockquiet/spawn/releases/latest");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-                if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                    plugin.getLogger().warning("Unable to check for updates...");
-                    return;
+                    if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                        plugin.getLogger().warning("Unable to check for updates...");
+                        return;
+                    }
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    JsonObject jsonResponse = new Gson().fromJson(bufferedReader, JsonObject.class);
+
+                    bufferedReader.close();
+                    con.disconnect();
+
+                    Version latest = Version.parse(jsonResponse.get("tag_name").getAsString());
+
+                    String pluginVersion = plugin.getDescription().getVersion();
+                    if (pluginVersion.contains("SNAPSHOT")) {
+                        plugin.getLogger().info(
+                                "You are running a development build, please report any bugs on the project's GitHub.");
+                        plugin.getLogger()
+                                .info("Latest release version: " + latest + ", you are using: " + pluginVersion);
+                        return;
+                    }
+
+                    Version current = Version.parse(pluginVersion);
+                    int compare = latest.compareTo(current);
+
+                    if (compare > 0) {
+                        plugin.getLogger().info(
+                                "An update is available! Latest version: " + latest + ", you are using: " + current);
+                    } else if (compare < 0) {
+                        plugin.getLogger().warning(
+                                "You are running a newer version of the plugin than released. If you are using a development build, please report any bugs on the project's GitHub.");
+                    }
+                } catch (IOException e) {
+                    plugin.getLogger().info("Unable to check for updates: " + e.getMessage());
                 }
+            });
+        else
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+                try {
+                    URL url = new URL("https://api.github.com/repos/rockquiet/spawn/releases/latest");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                JsonObject jsonResponse = new Gson().fromJson(bufferedReader, JsonObject.class);
+                    if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                        plugin.getLogger().warning("Unable to check for updates...");
+                        return;
+                    }
 
-                bufferedReader.close();
-                con.disconnect();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    JsonObject jsonResponse = new Gson().fromJson(bufferedReader, JsonObject.class);
 
-                Version latest = Version.parse(jsonResponse.get("tag_name").getAsString());
+                    bufferedReader.close();
+                    con.disconnect();
 
-                String pluginVersion = plugin.getDescription().getVersion();
-                if (pluginVersion.contains("SNAPSHOT")) {
-                    plugin.getLogger().info("You are running a development build, please report any bugs on the project's GitHub.");
-                    plugin.getLogger().info("Latest release version: " + latest + ", you are using: " + pluginVersion);
-                    return;
+                    Version latest = Version.parse(jsonResponse.get("tag_name").getAsString());
+
+                    String pluginVersion = plugin.getDescription().getVersion();
+                    if (pluginVersion.contains("SNAPSHOT")) {
+                        plugin.getLogger().info(
+                                "You are running a development build, please report any bugs on the project's GitHub.");
+                        plugin.getLogger()
+                                .info("Latest release version: " + latest + ", you are using: " + pluginVersion);
+                        return;
+                    }
+
+                    Version current = Version.parse(pluginVersion);
+                    int compare = latest.compareTo(current);
+
+                    if (compare > 0) {
+                        plugin.getLogger().info(
+                                "An update is available! Latest version: " + latest + ", you are using: " + current);
+                    } else if (compare < 0) {
+                        plugin.getLogger().warning(
+                                "You are running a newer version of the plugin than released. If you are using a development build, please report any bugs on the project's GitHub.");
+                    }
+                } catch (IOException e) {
+                    plugin.getLogger().info("Unable to check for updates: " + e.getMessage());
                 }
-
-                Version current = Version.parse(pluginVersion);
-                int compare = latest.compareTo(current);
-
-                if (compare > 0) {
-                    plugin.getLogger().info("An update is available! Latest version: " + latest + ", you are using: " + current);
-                } else if (compare < 0) {
-                    plugin.getLogger().warning("You are running a newer version of the plugin than released. If you are using a development build, please report any bugs on the project's GitHub.");
-                }
-            } catch (IOException e) {
-                plugin.getLogger().info("Unable to check for updates: " + e.getMessage());
-            }
-        });
+            });
     }
 }
